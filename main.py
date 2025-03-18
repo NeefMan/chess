caf0254f66354d6e63f36e3370d7ad9f6162c424
Ethan Lahr <ethanlahr334@gmail.com>
2025-03-17 21:12:45 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+import sys
+
+class Game:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+
+class Board:
+    def __init__(self):
+        pass
+
+
+class Piece:
+    def __init__(self):
+        pass
+
+
+class Settings:
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    main = Game()
+    main.run()
