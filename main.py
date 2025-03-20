302ffa4972bd65a34e8028ee4f4843e07776d764
Ethan Lahr <ethanlahr334@gmail.com>
2025-03-19 20:58:43 -0400

    initialize pieces, display pawns, introduced piece maps

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import pygame
 import sys
+import classic_piece_map
 
 class Game:
     def __init__(self):
         self.settings = Settings()
-        self.board = Board(self.settings.colors["wheat"], self.settings.colors["brown"])
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.board = Board(self.settings.colors["wheat"], self.settings.colors["brown"])
         self.clock = pygame.time.Clock()
         self.running = True
-        self.pawn = Pawn("images/pieces/white_pawn.png", 100, 100)
 
     def run(self):
         while self.running:
@@ -18,22 +18,11 @@ class Game:
                 if event.type == pygame.QUIT:
                     sys.exit()
             
-            self.display_board()
-            self.screen.blit(self.pawn.image, (400,400))
+            self.board.display_board(self.screen)
 
             pygame.display.flip()
             self.clock.tick(self.settings.fps)
-
-    def display_board(self):
-        for key, value in self.board.squares.items():
-            x, y = key
-            color = value["color"]
-            pygame.draw.rect(
-                self.screen, 
-                color, 
-                pygame.rect.Rect(x+self.board.board_x, y+self.board.board_y, self.board.square_width, self.board.square_height)
-                )
-
+  
 
 class Board:
     def __init__(self, light, dark):
@@ -53,6 +42,7 @@ class Board:
         self.squares = {} # (x, y) : {color, piece[None]}
         
         self.initialize_board()
+        self.initalize_pieces(classic_piece_map)
 
     def initialize_board(self):
         color1 = self.board_color_light
@@ -61,19 +51,46 @@ class Board:
             for x in range(0, self.board_width, self.square_width):
                 self.squares[(x,y)] = {}
                 self.squares[(x,y)]["color"] = color1
+                self.squares[(x,y)]["piece"] = None
                 if x < self.board_width - self.square_width:
                     color1, color2 = color2, color1
+    
+    def initalize_pieces(self, imported_piece_map):
+        image_prefix = "images/pieces/"
+        for key, value in imported_piece_map.piece_map.items():
+            x, y = key
+            x *= self.square_width
+            y *= self.square_height
+            if value == "wp":
+                self.squares[(x,y)]["piece"] = Pawn("w", f"{image_prefix}white_pawn.png", self.square_width, self.square_height)
+            elif value == "bp":
+                self.squares[(x,y)]["piece"] = Pawn("b", f"{image_prefix}black_pawn.png", self.square_width, self.square_height)
+
+    def display_board(self, screen):
+        for key, value in self.squares.items():
+            x, y = key
+            color = value["color"]
+            pygame.draw.rect(
+                screen, 
+                color, 
+                pygame.rect.Rect(x+self.board_x, y+self.board_y, self.square_width, self.square_height)
+                )
+            
+            piece = value["piece"]
+            if piece:
+                screen.blit(piece.image, (x,y))
 
 
 
 class Piece:
-    def __init__(self, image, desired_image_width, desired_image_height):
+    def __init__(self, color, image, desired_image_width, desired_image_height):
         self.image = pygame.transform.scale(pygame.image.load(image).convert_alpha(), (desired_image_width, desired_image_height))
+        self.color = color
 
 
 class Pawn(Piece):
-    def __init__(self, image, desired_image_width, desired_image_height):
-        super().__init__(image, desired_image_width, desired_image_height)
+    def __init__(self, color, image, desired_image_width, desired_image_height):
+        super().__init__(color, image, desired_image_width, desired_image_height)
 
 
 class Settings:
